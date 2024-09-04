edb964b5ded4b7f126c1626292d1d5aaf5b3f2df
0xatanda <atanda0x@gmail.com>
2024-09-03 20:34:40 -0700

    tailwind removed style with only css  and templ files generated

--- handlers/handler.go
+++ handlers/handler.go
@@ -1,21 +1,38 @@
 package handlers
 
 import (
-	"net/http"
+	"context"
 
 	"github.com/0xatanda/shantytown/templates"
 	"github.com/gin-gonic/gin"
 )
 
 func Home(c *gin.Context) {
-	templates.Home().Render(c.Writer)
+	ctx := context.Background()
+	err := templates.Home().Render(ctx, c.Writer)
+	if err != nil {
+		c.String(500, "Internal Server Error")
+	}
 }
 
 func CommunityMembers(c *gin.Context) {
-	members := []string{"member 1", "Member 2", "Member 3"}
-	c.JSON(http.StatusOK, members)
+	members := []string{
+		"https://picsum.photos/100?random=1",
+		"https://picsum.photos/100?random=2",
+		"https://picsum.photos/100?random=3",
+		// Add more random image URLs as needed
+	}
+
+	html := "<div class='profile-grid'>"
+	for _, member := range members {
+		html += "<div class='profile-circle'><img src='" + member + "' alt='Community Member'></div>"
+	}
+	html += "</div>"
+
+	c.Header("Content-Type", "text/html")
+	c.String(200, html)
 }
 
 func Join(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"Message": "Joined successfully"})
+	c.JSON(200, gin.H{"message": "Joined successfully"})
 }
