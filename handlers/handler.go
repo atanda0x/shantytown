dea3eb6ffa2514da63717ff0a7585b12676d6e2a
0xatanda <atanda0x@gmail.com>
2024-09-03 19:24:54 -0700

    handlers, main func

--- handlers/handler.go
+++ handlers/handler.go
@@ -1,9 +1,21 @@
 package handlers
 
 import (
+	"net/http"
+
+	"github.com/0xatanda/shantytown/templates"
 	"github.com/gin-gonic/gin"
 )
 
 func Home(c *gin.Context) {
+	templates.Home().Render(c.Writer)
+}
 
+func CommunityMembers(c *gin.Context) {
+	members := []string{"member 1", "Member 2", "Member 3"}
+	c.JSON(http.StatusOK, members)
+}
+
+func Join(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"Message": "Joined successfully"})
 }
--- main.go
+++ main.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/0xatanda/shantytown/handlers"
+	"github.com/0xatanda/shantytown/middleware"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load(".env")
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("Port isn't found in the env")
+	}
+
+	router := gin.New()
+
+	router.Use(middleware.CORS())
+	router.Static("/static", "./static")
+
+	router.GET("/", handlers.Home)
+
+	srv := &http.Server{
+		Addr:    ":" + portString,
+		Handler: router,
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal("Server is starting on port: ", portString)
+	}
+}
